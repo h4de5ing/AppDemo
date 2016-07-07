086bf75eeaf5cb44b87d98d91a335a8e8d68ccd7
Gh0st <lex109@qq.com>
2016-07-08 02:45:32 +0800

    [commit] Drawer

---
+++ drawer/src/main/java/com/code19/drawer/HomeBean.java
@@ -0,0 +1,26 @@
+package com.code19.drawer;
+
+/**
+ * Created by Administrator on 2016/7/0008.
+ */
+
+public class HomeBean {
+    private int icon;
+    private String name;
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
