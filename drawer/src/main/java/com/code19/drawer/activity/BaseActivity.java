854002c037716888776d11c0e6d1c698f8a905cd
Gh0st <lex109@qq.com>
2016-07-28 00:36:00 +0800

    app background

--- drawer/src/main/java/com/code19/drawer/activity/BaseActivity.java
+++ drawer/src/main/java/com/code19/drawer/activity/BaseActivity.java
@@ -2,6 +2,9 @@ package com.code19.drawer.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.code19.library.SystemUtils;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -19,5 +22,13 @@ public class BaseActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(homeButtonEnable);
         getSupportActionBar().setDisplayHomeAsUpEnabled(homeButtonEnable);
         getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (SystemUtils.isBackground(this)) {
+            Toast.makeText(this, "app进入后台运行", Toast.LENGTH_SHORT).show();
+        }
     }
 }
